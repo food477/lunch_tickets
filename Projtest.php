4b421d6c14d92aa8f2defa1f6655a401d639eb27
David G <DGomez@SalusCareFlorida.org>
2016-05-10 10:26:19 -0400

    Changes to variables and a function

--- Projtest.php
+++ Projtest.php
@@ -25,7 +25,7 @@
 <?php 
 
  /* attempts to connect to the 'kitchen" sql database */
-$serverName = "(local)\Kitchen";
+/*$serverName = "(local)\Kitchen";
 
 $connectionOptions = array("Kitchen"=>"SQL_SERVER_MANAGEMENT_STUDIO");
 
@@ -33,7 +33,7 @@ $connectionOptions = array("Kitchen"=>"SQL_SERVER_MANAGEMENT_STUDIO");
 
 /* Connect using Windows Authentication. */
 
-$conn = sqlsrv_connect($serverName, $connectionOptions);
+/*$conn = sqlsrv_connect($serverName, $connectionOptions);
 
 if( $conn === false )
 
@@ -43,8 +43,9 @@ if( $conn === false )
 	  
 	  
 	  /* Returns users id, want it to also return Name if return buyer */
-	  function getButton (){
-	   $Bpressed = 0;
+	  function getButton(){
+		  
+	   $Bpressed = null;
  if(isset($_POST['sub'])) {
    echo "<b> Your ID is </b>". $_POST['Cardnumb'];
 	$fdf = 'Cardnumb';
@@ -52,41 +53,46 @@ if( $conn === false )
       /* Buttons 1.00$ to 5.00$ */
  if (isset($_POST['button1'])) 
 { 
-   echo "1.00$ has been used ". $_POST['Cardnumb']; 
-   $Bpressed = 1;
+   
+   $Bpressed = $_POST['button1'];
 }  
 
 
-if (isset($_POST['button2'])) 
+ if (isset($_POST['button2'])) 
 { 
-   echo "2.00$ has been used ". $_POST['Cardnumb']; 
-   $Bpressed = 2;
+   
+   $Bpressed = $_POST['button2'];
 } 
 
  if (isset($_POST['button3'])) 
 { 
-   echo "3.00$ has been used ". $_POST['Cardnumb']; 
-   $Bpressed = 3;
+   
+   $Bpressed = $_POST['button3'];
 } 
 
  if (isset($_POST['button4'])) 
 { 
-   echo "4.00$ has been used ". $_POST['Cardnumb']; 
-   $Bpressed = 4;
+   
+   $Bpressed = $_POST['button4'];
 }  
 
-elseif (isset($_POST['button5'])) 
+ elseif (isset($_POST['button5'])) 
 { 
-   echo "5.00$ has been used ". $_POST['Cardnumb']; 
-   $Bpressed = 5;
+    
+   $Bpressed = $_POST['button5'];
 }  
  
- 
-	  }
-$log_ info = date("m/d/Y, h:i:s A") . " - " . $_POST['Cardnumb'] . " utilized dollar amount";
+ return $Bpressed;
+	  
+/* $log_info = date("m/d/Y, h:i:s A") . " - " . $_POST['Cardnumb'] . " utilized dollar amount";
 $save_name = fopen('lunch_log.txt', 'a');
 fwrite($save_name,$log_info);
-fclose($save_name);
+fclose($save_name); */
+	  }
+	  $Bpressed = null;
+	  getButton();
+	  
+	  echo $Bpressed ." has been used by ". $_POST['Cardnumb'];
 	  
 ?>
 </body>
